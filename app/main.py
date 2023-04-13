4f59e3497e53514cfec0474265e591d15fbe23df
Bernard <bernard@DS-N-0216-dongwoonson.local>
2023-04-14 00:03:53 +0900

    Filesystem isolation

--- app/main.py
+++ app/main.py
@@ -1,12 +1,28 @@
+import os
+import shutil
 import subprocess
 import sys
+import tempfile
+
+ROOT_PATH = os.path.abspath(os.path.sep)
+
+
+def relpath_from_root(path: str) -> str:
+    return os.path.relpath(path, ROOT_PATH)
+
+
+def newroot_path(path: str, temp_dir: str) -> str:
+    return os.path.join(temp_dir, relpath_from_root(path))
 
 
 def main() -> None:
     command = sys.argv[3]
     args = sys.argv[4:]
 
-    completed_process = subprocess.run([command, *args], capture_output=True)
+    with tempfile.TemporaryDirectory() as temp_dir:
+        os.makedirs(newroot_path(os.path.dirname(command), temp_dir))
+        shutil.copy(command, newroot_path(command, temp_dir))
+        completed_process = subprocess.run(["chroot", temp_dir, command, *args], capture_output=True)
 
     sys.stdout.buffer.write(completed_process.stdout)
     sys.stderr.buffer.write(completed_process.stderr)
